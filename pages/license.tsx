9e377f9cfba2b776c7e5fed7895e066f987e5111
alsonick <contactnicholas81@gmail.com>
2022-03-01 14:04:38 +0000

    Add license page

---
+++ pages/license.tsx
@@ -0,0 +1,56 @@
+import { Heading } from "../components/Heading";
+import { Animate } from "../components/Animate";
+import { GoBack } from "../components/GoBack";
+import { Layout } from "../components/Layout";
+import { Seo } from "../components/Seo";
+
+// Next.js
+import { NextPage } from "next";
+import { Text } from "../components/Text";
+
+const License: NextPage = () => {
+  return (
+    <>
+      <Seo
+        title="License - Nicholas Njoki"
+        description="Nicholas Njoki - Full-Stack Developer"
+      />
+      <Layout>
+        <Animate>
+          <div className="flex items-center mb-4 min-h-[4rem] justify-between">
+            <Heading style={{ marginBottom: 0 }}>License</Heading>
+          </div>
+          <div className="mb-4">
+            <Text>
+              MIT License <br />
+              <br /> Copyright (c) {new Date().getFullYear()} Nicholas Njoki
+              <br />
+              <br /> Permission is hereby granted, free of charge, to any person
+              obtaining a copy of this software and associated documentation
+              files (the &quot;Software&quot;), to deal in the Software without
+              restriction, including without limitation the rights to use, copy,
+              modify, merge, publish, distribute, sublicense, and/or sell copies
+              of the Software, and to permit persons to whom the Software is
+              furnished to do so, subject to the following conditions:
+              <br />
+              <br /> The above copyright notice and this permission notice shall
+              be included in all copies or substantial portions of the Software.
+              <br />
+              <br /> THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT
+              WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+              LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+              PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+              OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+              OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+              OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+            </Text>
+          </div>
+          <GoBack />
+        </Animate>
+      </Layout>
+    </>
+  );
+};
+
+export default License;
