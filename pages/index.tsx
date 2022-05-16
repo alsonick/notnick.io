a618ac20707fed8ca046d4ceab7a4ddefc7209d3
Nicholas <contactnicholas81@gmail.com>
2022-05-16 15:56:56 +0100

    add favicon and improve seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,19 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Nicholas Njoki</title>
+        <meta name="theme-color" content="#f54bff" />
+        <meta
+          name="description"
+          content="Nicholas Njoki - Full-Stack Developer"
+        />
+        <meta name="twitter:title" content="Nicholas Njoki" />
+        <meta
+          name="twitter:description"
+          content="Nicholas Njoki - Full-Stack Developer"
+        />
       </Head>
       <div className="flex flex-col items-center lg:items-start px-5 lg:px-10 dark:bg-black">
-        <div className="flex flex-col h-full justify-center lg:w-[35rem]">
+        <div className="flex flex-col h-screen justify-center lg:w-[35rem]">
           <Main />
           <Footer />
         </div>
