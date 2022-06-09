84546fb4245a4c20e97d6bbc4539b0371bc22f6a
Nicholas <contactnicholas81@gmail.com>
2022-06-09 10:24:17 +0100

    fix

--- components/NoPost.tsx
+++ components/NoPost.tsx
@@ -2,7 +2,7 @@ export const NoPost = ({ type }: { type: "notes" | "blogs" }) => {
   return (
     <div className="text-center h-full items-center justify-center flex mb-4">
       <h2 className="text-xl font-semibold opacity-30 dark:text-white">
-        I've not written any {type === "notes" ? "notes" : "blogs"} yet.
+        I&apos;ve not written any {type === "notes" ? "notes" : "blogs"} yet.
       </h2>
     </div>
   );
