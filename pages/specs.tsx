cbbf476e04ea45aa80e7385fb224c6c36a1b36d6
unknown <contactnicholas81@gmail.com>
2022-08-11 19:21:26 +0100

    Add gears

--- pages/specs.tsx
+++ pages/specs.tsx
@@ -15,11 +15,29 @@ const Specs: NextPage = () => {
           <Heading style={{ fontSize: "2rem", marginBottom: 0 }}>PC</Heading>
           <ul className="mt-3">
             <li>
-              <Text>Processor: Intel Core i9-11900KF</Text>
-              <Text>RAM: 32GB</Text>
-              <Text>GPU: NVIDIA GeForce RTX 3060</Text>
-              <Text>OS: Windows 11</Text>
-              <Text>Storage: 1TB SSD</Text>
+              <Text>
+                <Key>Processor:</Key> Intel Core i9-11900KF
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>RAM:</Key> 32GB
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>GPU:</Key> NVIDIA GeForce RTX 3060
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>OS:</Key> Windows 11
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Storage:</Key> 1TB SSD
+              </Text>
             </li>
           </ul>
         </Section>
@@ -29,16 +47,78 @@ const Specs: NextPage = () => {
           </Heading>
           <ul className="mt-3">
             <li>
-              <Text>Display: Retina display</Text>
-              <Text>Chip: Apple M1 chip</Text>
-              <Text>Memory: 8GB</Text>
-              <Text>Storage: 256GB SSD</Text>
+              <Text>
+                <Key>Display:</Key> Retina display
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Chip:</Key> Apple M1 chip
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Memory:</Key> 8GB
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Storage:</Key> 256GB SSD
+              </Text>
+            </li>
+          </ul>
+        </Section>
+        <Section>
+          <Heading style={{ fontSize: "2rem", marginBottom: 0 }}>Gear</Heading>
+          <ul className="mt-3">
+            <li>
+              <Text>
+                <Key>Monitor:</Key> Samsung UR55 4K IPS HDR10
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Keyboard:</Key> Keychron K4 Wireless Mechanical Keyboard
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Mouse:</Key> TECKNET RGB Gaming Mouse
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Headset:</Key> HyperX Cloud II 7.1
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Earbuds:</Key> AirPods Pro
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Watch: </Key>
+                Apple Watch Series 3
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Key>Phone: </Key>
+                iPhone XR
+              </Text>
             </li>
           </ul>
         </Section>
         <LinkTag href="/">&larr; Home page</LinkTag>
       </Layout>
     </>
+  );
+};
+
+const Key = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <span className="text-black dark:text-white font-bold">{children}</span>
   );
 };
 
