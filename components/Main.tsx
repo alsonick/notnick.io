bb495d7777b177e0b5b3a9029136e619c8906d3c
unknown <contactnicholas81@gmail.com>
2022-09-04 20:33:24 +0100

    Small change

--- components/Main.tsx
+++ components/Main.tsx
@@ -13,7 +13,7 @@ export const Main = () => {
       <main className="flex md:flex-row md:flex-start flex-col-reverse items-start md:items-center w-full justify-between mb-12 mt-20">
         <div className="flex flex-col">
           <Link href={`https://www.google.com/maps/place/${CITY}`}>
-            <a className="w-fit" target="_blank">
+            <a className="w-fit font-semibold" target="_blank">
               <div className="flex items-center p-1 text-sm px-3 mb-2 w-fit rounded-full text-white bg-[#f54bff]">
                 <FiSend className="mr-2" />
                 {CITY}, {COUNTRY}
