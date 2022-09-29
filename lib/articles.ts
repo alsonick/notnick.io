2d8f7a1ef0a6e56fad488079db07eb2f81cca4d0
alsonick <contactnicholas81@gmail.com>
2022-09-29 20:19:47 +0100

    articles page

---
+++ lib/articles.ts
@@ -0,0 +1,14 @@
+export const ARTICLES = [
+  {
+    id: 1,
+    title: "Why Google stores billions of lines of code in a single repository",
+    link: "https://dl.acm.org/doi/fullHtml/10.1145/2854146",
+    authors: ["Rachel Potvin", "Josh Levenberg"],
+  },
+  {
+    id: 2,
+    title: "The 50/30/20 rule: how to budget your money more efficiently",
+    link: "https://n26.com/en-eu/blog/50-30-20-rule",
+    authors: ["N26"],
+  },
+];
---
+++ pages/articles.tsx
@@ -0,0 +1,75 @@
+import { Heading } from "../components/Heading";
+import { GoBack } from "../components/GoBack";
+import { Layout } from "../components/Layout";
+import { ARTICLES } from "../lib/articles";
+import { Text } from "../components/Text";
+import { Seo } from "../components/Seo";
+
+// Next.js
+import { NextPage } from "next";
+import Link from "next/link";
+
+const Articles: NextPage = () => {
+  return (
+    <>
+      <Seo
+        title="Articles - Nicholas Njoki"
+        description="Nicholas Njoki - Full-Stack Developer"
+      />
+      <Layout>
+        <div className="flex flex-col mb-4 min-h-[4rem] justify-between">
+          <Heading style={{ marginBottom: 0 }}>Articles</Heading>
+          <Text>A list of interesting articles I found on the internet.</Text>
+        </div>
+        <div className="flex flex-col mb-4">
+          {ARTICLES.map((article) => (
+            <div
+              className="border-b border-teal-100 dark:border-teal-900
+              hover:no-underline py-4 w-full"
+              key={article.id}
+            >
+              <Link href={article.link}>
+                <a
+                  className="text-3xl font-bold cursor-pointer underline
+                  dark:text-white hover:no-underline"
+                  target="_blank"
+                >
+                  {article.title}
+                </a>
+              </Link>
+              <div className="flex w-full">
+                {article.authors.length === 1 ? (
+                  <Text style={{ margin: "0.5rem 0 0.25rem 0" }}>
+                    By {article.authors[0]}
+                  </Text>
+                ) : (
+                  <>
+                    <div
+                      className="flex"
+                      style={{ margin: "0.5rem 0 0.25rem 0" }}
+                    >
+                      <Text>By&nbsp;</Text>
+                      {article.authors.map((author, index) => (
+                        <>
+                          {article.authors[article.authors.length - 1]
+                            .length !== article.authors[index].length ? (
+                            <Text key={article.id}>{author},&nbsp;</Text>
+                          ) : (
+                            <Text key={article.id}>{author}&nbsp;</Text>
+                          )}
+                        </>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <GoBack />
+      </Layout>
+    </>
+  );
+};
+//
+export default Articles;
