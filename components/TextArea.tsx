0bfe62a6b027ac16eb7b0e3b1f5a70db1232f094
Nicholas <contactnicholas81@gmail.com>
2022-10-30 19:52:46 +0000

    disable resize

--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -8,7 +8,7 @@ export const TextArea = (
     <textarea
       className="lex items-center p-2 rounded-lg justify-center px-4 text-black
   dark:text-white duration-300 font-semibold bg-gray-100
-  dark:bg-gray-800 focus:ring-2 ring-[#f54bff] outline-none"
+  dark:bg-gray-800 focus:ring-2 ring-[#f54bff] resize-none outline-none"
       {...props}
     />
   );
