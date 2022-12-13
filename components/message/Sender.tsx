c7c7ffa25063ce3c6feedbf282543636e4d1f9bd
Nicholas <contactnicholas81@gmail.com>
2022-12-13 19:27:28 +0000

    delete components i dont need

--- components/message/Sender.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  sender: string;
-}
-
-export const Sender = ({ sender }: Props) => {
-  return <h2 className="dark:text-white font-semibold">{sender}</h2>;
-};
--- components/message/Text.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  text: string;
-}
-
-export const Text = ({ text }: Props) => {
-  return <p className="text-gray-600 dark:text-gray-300 text-sm">{text}</p>;
-};
