1d5407f050b39099b8dfb1d115599ff725625684
Nicholas <contactnicholas81@gmail.com>
2023-01-09 10:49:21 +0000

    improve seo and add og image

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,6 +1,9 @@
+import { dynamicTitle } from "../lib/dynamic-title";
 import { fireworks } from "../lib/fireworks";
+import { useEffect, useState } from "react";
 
 // Next.js
+import { useRouter } from "next/router";
 import Script from "next/script";
 import Head from "next/head";
 
@@ -10,7 +13,11 @@ interface Props {
 }
 
 export const Seo = ({ description, title }: Props) => {
+  const [validPostPathState, setValidPostPathState] = useState<boolean>(false);
+
   const date = new Date();
+
+  const router = useRouter();
 
   if (typeof window === "object") {
     if (
@@ -22,6 +29,23 @@ export const Seo = ({ description, title }: Props) => {
     }
   }
 
+  const validPostPath = () => {
+    const pathname = router.pathname;
+
+    if (pathname.includes("notes") || pathname.includes("blogs")) {
+      console.log("includes note or blog");
+      setValidPostPathState(true);
+      return;
+    }
+
+    setValidPostPathState(false);
+  };
+
+  useEffect(() => {
+    validPostPath();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Head>
@@ -32,15 +56,33 @@ export const Seo = ({ description, title }: Props) => {
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:creator" content="@heynickn" />
         <meta property="twitter:site" content="@nick" />
         <meta property="og:title" content={title} />
+        <meta property="og:site_name" content="Nicholas Njoki" />
         <meta property="og:url" content="https://notnick.io/" />
         <meta property="og:description" content={description} />
         <meta property="og:type" content="website" />
-        <meta property="twitter:image" content="/cover.png" />
-        <meta property="og:image" content="/cover.png" />
+        <>
+          {validPostPathState ? (
+            <>
+              <meta property="twitter:card" content="summary_large_image" />
+              <meta property="twitter:image" content={dynamicTitle(title)} />
+              <meta property="og:image" content={dynamicTitle(title)} />
+            </>
+          ) : (
+            <>
+              <meta
+                property="twitter:image"
+                content={"https://notnick.io/pfp.png"}
+              />
+              <meta
+                property="og:image"
+                content={"https://notnick.io/pfp.png"}
+              />
+            </>
+          )}
+        </>
       </Head>
       <Script async defer src="https://buttons.github.io/buttons.js" />
       {date.getMonth() + 1 >= 12 && date.getDate() >= 1 ? (
---
+++ pages/api/og.tsx
@@ -0,0 +1,74 @@
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+import { Tag } from "../../components/Tag";
+export const config = {
+  runtime: "edge",
+};
+
+const font = fetch(
+  new URL("../../assets/Inter-Bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export default async function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const hasDescription = searchParams.has("description");
+    const hasTitle = searchParams.has("title");
+
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "Nicholas Njoki";
+
+    const description = hasDescription
+      ? searchParams.get("description")?.slice(0, 100)
+      : "No description.";
+
+    console.log(description);
+
+    const fontData = await font;
+    // px - 15;
+    return new ImageResponse(
+      (
+        <div tw="flex w-full items-center bg-black py-30 text-white text-2xl h-full">
+          <div tw="absolute top-0 bg-[#f54bff] h-5 w-full z-20"></div>
+          <div tw="px-30 flex flex-col h-full justify-between">
+            <div tw="flex flex-col my-10">
+              <h1 tw="text-7xl tracking-tight m-0 mb-5">{title}</h1>
+              <p tw="text-gray-600 max-w-2xl m-0">{description}</p>
+            </div>
+            <div tw="flex items-center">
+              <img
+                tw="rounded-full mr-5"
+                src="https://notnick.io/pfp.png"
+                width={90}
+                height={90}
+                alt="sd"
+              />
+              <div tw="flex flex-col justify-center">
+                <h2 tw="text-4xl m-0 tracking-tight">Nicholas Njoki</h2>
+                <p tw="text-gray-600 text-lg m-0">Full-Stack Developer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 600,
+        fonts: [
+          {
+            name: "Inter",
+            data: fontData,
+            style: "normal",
+          },
+        ],
+      }
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
