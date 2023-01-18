ceb315d68a0a477ab4f2b3831d72a83c44a8cf6b
Nicholas <contactnicholas81@gmail.com>
2023-01-18 18:38:40 +0000

    update goals

--- pages/goals/2023.tsx
+++ pages/goals/2023.tsx
@@ -25,22 +25,23 @@ const TZTT = () => {
       <Heading>Goals for 2023</Heading>
       <ul className="pl-10">
         <Item completed={false}>
-          Make at least 3000 contributions on GitHub.
-        </Item>
-        <Item completed={false}>
           Become a TS god and surpass{" "}
           <LinkT href="https://twitter.com/alistaiir" target="_blank">
             Alistair
           </LinkT>
           .
         </Item>
-        <Item completed={false}>Learn and get more into networking.</Item>
         <Item completed={false}>Learn PHP and master JavaScript.</Item>
         <Item completed={true}>Spend less time on social media.</Item>
         <Item completed={false}>Contribute more to open source.</Item>
+        <Item completed={false}>Get my driving license.</Item>
+        <Item completed={false}>Learn and get more into networking.</Item>
         <Item completed={false}>Meet more like-minded people.</Item>
         <Item completed={false}>Hit the gym more consistently.</Item>
-        <Item completed={false}>Spend less time on my phone.</Item>
+        <Item completed={false}>
+          Make at least 3000 contributions on GitHub.
+        </Item>
+        <Item completed={true}>Spend less time on my phone.</Item>
         <Item completed={false}>
           Ship products like{" "}
           <LinkT href="https://twitter.com/levelsio" target="_blank">
@@ -48,9 +49,9 @@ const TZTT = () => {
           </LinkT>
           .
         </Item>
-        <Item completed={false}>Improve my sleep schedule.</Item>
-        <Item completed={false}>Listen to more podcasts.</Item>
-        <Item completed={false}>Write more blogs.</Item>
+        <Item completed={true}>Write more blogs.</Item>
+        <Item completed={true}>Improve my sleep schedule.</Item>
+        <Item completed={true}>Listen to more podcasts.</Item>
       </ul>
     </div>
   );
