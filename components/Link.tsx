11377189bbe61fcb5013134bcbbc07c8e0670bad
Nicholas <contactnicholas81@gmail.com>
2024-01-01 21:42:09 +0000

    Small fix

--- components/Link.tsx
+++ components/Link.tsx
@@ -25,7 +25,7 @@ export const LinkT = ({
       href={href || ""}
       title={title}
       className={`font-bold focus:ring-offset-2 dark:ring-offset-black rounded outline-none focus:ring-[#f54bff] 
-      items-center inline-flex w-fit duration-300 cursor-pointer text-[${THEME}] hover:underline focus:ring-4`}
+      items-center inline-flex w-fit duration-300 cursor-pointer text-[#f54bff] hover:underline focus:ring-4`}
       onClick={onClick}
       style={style}
       target={target}
