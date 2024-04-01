14773eff473724ea67f5fe007fab0e025a9f541b
Nicholas <contactnicholas81@gmail.com>
2024-04-01 21:30:03 +0100

    18 done babby

--- lib/challenges/100-days-of-swift/index.ts
+++ lib/challenges/100-days-of-swift/index.ts
@@ -279,4 +279,25 @@ export const ONE_HUNDRED_DAYS_OF_SWIFT: Content[] = [
     },
     day: 17,
   },
+  {
+    id: generateRandomId(),
+    description: "Project 1, part three",
+    slug: "100-days-of-swift",
+    completed: true,
+    code: {
+      icon: "xcode",
+      image: {
+        extension: "png",
+      },
+      link: `${social.github.link}/100-days-of-swift/tree/main/Day16-17-18/Project1`,
+    },
+    preview: {
+      available: true,
+      previewContent: {
+        path: "/challenge/100-days-of-swift/Day",
+        extension: "gif",
+      },
+    },
+    day: 18,
+  },
 ];
