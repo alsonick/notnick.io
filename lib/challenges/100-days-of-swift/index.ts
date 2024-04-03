1966f8fdd46566012dc7b1cc0488a32dea86e6a7
Nicholas <contactnicholas81@gmail.com>
2024-04-03 15:09:39 +0100

    20 pog

--- lib/challenges/100-days-of-swift/index.ts
+++ lib/challenges/100-days-of-swift/index.ts
@@ -321,4 +321,25 @@ export const ONE_HUNDRED_DAYS_OF_SWIFT: Content[] = [
     },
     day: 19,
   },
+  {
+    id: generateRandomId(),
+    description: "Project 2, part two",
+    slug: "100-days-of-swift",
+    completed: true,
+    code: {
+      icon: "xcode",
+      image: {
+        extension: "png",
+      },
+      link: `${social.github.link}/100-days-of-swift/tree/main/Day19-20-21/Project2`,
+    },
+    preview: {
+      available: true,
+      previewContent: {
+        path: "/challenge/100-days-of-swift/Day",
+        extension: "png",
+      },
+    },
+    day: 20,
+  },
 ];
