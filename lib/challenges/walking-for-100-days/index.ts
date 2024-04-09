b833247b98cf7628f4efff83f2a338f616a9565a
Nicholas <contactnicholas81@gmail.com>
2024-04-09 19:35:16 +0100

    forgot to commit yesterday

--- lib/challenges/walking-for-100-days/index.ts
+++ lib/challenges/walking-for-100-days/index.ts
@@ -47,4 +47,18 @@ export const WALKING_FOR_100_DAYS: Content[] = [
     },
     day: 3,
   },
+  {
+    id: generateRandomId(),
+    description: "Went to the gym today and worked on chest.",
+    slug: "walking-for-100-days",
+    completed: true,
+    preview: {
+      available: true,
+      previewContent: {
+        path: "Day",
+        extension: "jpeg",
+      },
+    },
+    day: 4,
+  },
 ];
