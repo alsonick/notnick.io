7e3c46e95d4e6e2e459d81b84f8f1b5e60b15c41
Nicholas <contactnicholas81@gmail.com>
2024-04-10 22:30:41 +0100

    i can sleep now

--- lib/challenges/walking-for-100-days/index.ts
+++ lib/challenges/walking-for-100-days/index.ts
@@ -76,4 +76,18 @@ export const WALKING_FOR_100_DAYS: Content[] = [
     },
     day: 5,
   },
+  {
+    id: generateRandomId(),
+    description: "Yet another walking day, feeling great! :D",
+    slug: "walking-for-100-days",
+    completed: true,
+    preview: {
+      available: true,
+      previewContent: {
+        path: "Day",
+        extension: "jpeg",
+      },
+    },
+    day: 6,
+  },
 ];
