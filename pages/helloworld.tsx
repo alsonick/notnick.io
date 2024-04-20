bd522bde083eee49a4b03f1444e783d885186053
Nicholas <contactnicholas81@gmail.com>
2024-04-20 11:14:10 +0100

    add seo

--- pages/helloworld.tsx
+++ pages/helloworld.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from "next";
+import { Seo } from "../components/Seo";
 
 const HelloWorld: NextPage = () => {
   return (
-    <h1 className="text-black dark:text-white p-10 font-serif text-4xl">
-      Hello, World!
-    </h1>
+    <div>
+      <Seo title="Nicholas Njoki" description="Hello, World!" />
+      <h1 className="text-black dark:text-white p-10 font-serif text-4xl">
+        Hello, World!
+      </h1>
+    </div>
   );
 };
 
