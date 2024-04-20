0bf1a9f0e0fa504f8bcc99e8ac856000ac631ffd
Nicholas <contactnicholas81@gmail.com>
2024-04-20 11:09:10 +0100

    hello world

---
+++ pages/helloworld.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const HelloWorld: NextPage = () => {
+  return (
+    <h1 className="text-black dark:text-white p-10 font-serif text-4xl">
+      Hello, World!
+    </h1>
+  );
+};
+
+export default HelloWorld;
