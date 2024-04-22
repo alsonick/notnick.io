d52ec4d373e23555f627ee4fc09ac75ed8fb9bb7
Nicholas <contactnicholas81@gmail.com>
2024-04-22 18:45:48 +0100

    39 done baby

--- lib/challenges/100-days-of-swift/index.ts
+++ lib/challenges/100-days-of-swift/index.ts
@@ -716,4 +716,21 @@ export const ONE_HUNDRED_DAYS_OF_SWIFT: Content[] = [
     },
     day: 38,
   },
+  {
+    id: generateRandomId(),
+    description: "Project 9, part one",
+    slug: "100-days-of-swift",
+    completed: true,
+    code: {
+      icon: "xcode",
+      image: {
+        extension: "png",
+      },
+      link: `${social.github.link}/100-days-of-swift/tree/main/Day39-40/Project9`,
+    },
+    preview: {
+      available: false,
+    },
+    day: 39,
+  },
 ];
