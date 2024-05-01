35b6be786b2f90c699d4a2017603039e26e41153
Nicholas <contactnicholas81@gmail.com>
2024-05-01 19:46:30 +0100

    27 done

--- lib/challenges/walking-for-100-days/index.ts
+++ lib/challenges/walking-for-100-days/index.ts
@@ -372,4 +372,18 @@ export const WALKING_FOR_100_DAYS: Content[] = [
     },
     day: 26,
   },
+  {
+    id: generateRandomId(),
+    description: "",
+    slug: "walking-for-100-days",
+    completed: true,
+    preview: {
+      available: true,
+      previewContent: {
+        path: "Day",
+        extension: "jpeg",
+      },
+    },
+    day: 27,
+  },
 ];
