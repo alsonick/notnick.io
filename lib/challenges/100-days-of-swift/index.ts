b8286b57c3174536ea874d2244df671f23a34197
Nicholas <contactnicholas81@gmail.com>
2024-05-03 17:25:23 +0100

    almost there!

--- lib/challenges/100-days-of-swift/index.ts
+++ lib/challenges/100-days-of-swift/index.ts
@@ -927,4 +927,25 @@ export const ONE_HUNDRED_DAYS_OF_SWIFT: Content[] = [
     },
     day: 49,
   },
+  {
+    id: generateRandomId(),
+    description: "Milestone: Projects 10-12",
+    slug: "100-days-of-swift",
+    completed: true,
+    code: {
+      icon: "xcode",
+      image: {
+        extension: "png",
+      },
+      link: `${social.github.link}/100-days-of-swift/tree/main/Day50/Challenge`,
+    },
+    preview: {
+      available: true,
+      previewContent: {
+        path: "Day",
+        extension: "gif",
+      },
+    },
+    day: 50,
+  },
 ];
