3dfefc9e31085048aa3f4921c3a0c93ea92d9b0a
Nicholas <contactnicholas81@gmail.com>
2024-05-15 20:29:05 +0100

    rags not paths

--- pages/api/rags.ts
+++ pages/api/rags.ts
@@ -2,5 +2,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { RAGS } from "../../lib/rags";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ success: true, paths: RAGS });
+  res.status(200).json({ success: true, rags: RAGS });
 }
