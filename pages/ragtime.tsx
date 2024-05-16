0caed8e23627df47942be9468e83f2bc5d28d019
Nicholas <contactnicholas81@gmail.com>
2024-05-16 18:34:54 +0100

    start on writing

--- pages/ragtime.tsx
+++ pages/ragtime.tsx
@@ -20,19 +20,18 @@ const Ragtime: NextPage = () => {
       />
       <Layout>
         <Animate>
-          <div className="mb-4 mt-8 flex w-full flex-col">
+          <div className="mb-4 mt-8 flex w-full flex-col font-serif">
             <div className="flex flex-col w-full items-center mb-10">
               <picture className="w-full mb-2 flex justify-center">
                 <img
-                  className="w-1/2"
-                  src="/assets/scott_joplin_portrait.jpeg"
+                  src="/assets/scott_joplin.png"
                   title="Portrait of Scott Joplin"
                   alt="Scott Joplin"
                 />
               </picture>
               <Text>Scott Joplin (1868-1917)</Text>
               <Text>
-                <strong>King of Ragtime 👑</strong>
+                <strong>King of Ragtime</strong>
               </Text>
             </div>
             <RagtimeSection title="What is Ragtime?">
@@ -43,12 +42,28 @@ const Ragtime: NextPage = () => {
                 composed three or four contrasting sections or strains, each one
                 being 16 or 32 measures in length.
               </Text>
+
+              <br />
+
+              <Text>Ragtime was typically played in saloons</Text>
             </RagtimeSection>
             <RagtimeSection title="Who is Scott Joplin?">
               <Text>
                 Scott Joplin was an American composer and pianist. Dubbed the
                 &quot;King of Ragtime&quot;, he composed more than 40 ragtime
                 pieces, one ragtime ballet, and two operas.
+              </Text>
+
+              <br />
+
+              <Text>
+                Scott Joplin was born around the years 1867 and 1868 in
+                Texarkana, he was the second of six children, born to Giles
+                Joplin, a former slave. Joplin was born into a musical family,
+                his father played the violin and his mother sang and played the
+                banjo. By the age of seven Joplin would start practicing on the
+                piano, and at some point he was able to receive a musical
+                education from a man called Julius Weiss
               </Text>
             </RagtimeSection>
             <RagtimeSection title="What is syncopation?">
