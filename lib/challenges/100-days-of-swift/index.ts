2fff598f42dd4ba99e7e7c85aca5fc9a2bda28ef
Nicholas <contactnicholas81@gmail.com>
2024-06-08 15:15:28 +0100

    missed commit yesterday

--- lib/challenges/100-days-of-swift/index.ts
+++ lib/challenges/100-days-of-swift/index.ts
@@ -1540,4 +1540,25 @@ export const ONE_HUNDRED_DAYS_OF_SWIFT: Content[] = [
     },
     day: 84,
   },
+  {
+    id: generateRandomId(),
+    description: "Project 26, part one",
+    slug: "100-days-of-swift",
+    completed: true,
+    code: {
+      icon: "xcode",
+      image: {
+        extension: "png",
+      },
+      link: `${social.github.link}/100-days-of-swift/tree/main/Day85-86-87/Project26`,
+    },
+    preview: {
+      available: true,
+      previewContent: {
+        path: "Day",
+        extension: "png",
+      },
+    },
+    day: 85,
+  },
 ];
