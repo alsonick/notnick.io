067aac7dbfd8b08cccfd035dc281d4f0bbf22eee
Nicholas <contactnicholas81@gmail.com>
2024-06-09 18:48:25 +0100

    2 weeks left

--- lib/challenges/100-days-of-swift/index.ts
+++ lib/challenges/100-days-of-swift/index.ts
@@ -1561,4 +1561,25 @@ export const ONE_HUNDRED_DAYS_OF_SWIFT: Content[] = [
     },
     day: 85,
   },
+  {
+    id: generateRandomId(),
+    description: "Project 26, part two",
+    slug: "100-days-of-swift",
+    completed: true,
+    code: {
+      icon: "xcode",
+      image: {
+        extension: "png",
+      },
+      link: `${social.github.link}/100-days-of-swift/tree/main/Day85-86-87/Project26`,
+    },
+    preview: {
+      available: true,
+      previewContent: {
+        path: "Day",
+        extension: "gif",
+      },
+    },
+    day: 86,
+  },
 ];
