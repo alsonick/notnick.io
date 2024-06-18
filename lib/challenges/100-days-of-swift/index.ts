291e4d75e5d2e715960f05f8892c48a53f9b9ce1
Nicholas <contactnicholas81@gmail.com>
2024-06-18 17:15:26 +0100

    30 done

--- lib/challenges/100-days-of-swift/index.ts
+++ lib/challenges/100-days-of-swift/index.ts
@@ -1726,4 +1726,25 @@ export const ONE_HUNDRED_DAYS_OF_SWIFT: Content[] = [
     },
     day: 96,
   },
+  {
+    id: generateRandomId(),
+    description: "Project 30, part one",
+    slug: "100-days-of-swift",
+    completed: true,
+    code: {
+      icon: "xcode",
+      image: {
+        extension: "png",
+      },
+      link: `${social.github.link}/100-days-of-swift/tree/main/Day97-98/Project30`,
+    },
+    preview: {
+      available: true,
+      previewContent: {
+        path: "Day",
+        extension: "png",
+      },
+    },
+    day: 97,
+  },
 ];
