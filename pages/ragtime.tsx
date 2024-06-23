c84b5c7cc46bc33e1a577f56f099e3f1ef9ad752
Nicholas <contactnicholas81@gmail.com>
2024-06-23 22:07:14 +0100

    fix page layout

--- pages/ragtime.tsx
+++ pages/ragtime.tsx
@@ -20,57 +20,60 @@ const Ragtime: NextPage = () => {
       />
       <Layout>
         <Animate>
-          <div className="mb-4 mt-8 flex w-full flex-col font-serif">
-            <div className="flex flex-col w-full items-center mb-10">
-              <picture className="w-full mb-2 flex justify-center">
-                <img
-                  src="/assets/scott_joplin.png"
-                  title="Portrait of Scott Joplin"
-                  alt="Scott Joplin"
-                />
-              </picture>
-              <Text>Scott Joplin (1868-1917)</Text>
-              <Text>
-                <strong>King of Ragtime</strong>
-              </Text>
+          <div className="mb-4 mt-20 flex w-full flex-col font-serif">
+            <div className="flex flex-row-reverse">
+              <div className="flex w-fit flex-col text-center h-fit items-center">
+                <picture className="mb-2 w-44 flex justify-center">
+                  <img
+                    src="/assets/scott_joplin.png"
+                    title="Portrait of Scott Joplin"
+                    alt="Scott Joplin"
+                  />
+                </picture>
+                <Text>
+                  Scott Joplin
+                  <br />
+                  (1868-1917)
+                </Text>
+              </div>
+              <div className="flex flex-col">
+                <RagtimeSection title="What is Ragtime?">
+                  <Text>
+                    A genre of musical composition for the piano, generally in
+                    duple meter and containing a highly syncopated treble lead
+                    over a rhythmically steady bass. A ragtime composition is
+                    usually composed three or four contrasting sections or
+                    strains, each one being 16 or 32 measures in length.
+                  </Text>
+                  <br />
+                  <Text>Ragtime was typically played in saloons</Text>
+                </RagtimeSection>
+              </div>
             </div>
-            <RagtimeSection title="What is Ragtime?">
-              <Text>
-                A genre of musical composition for the piano, generally in duple
-                meter and containing a highly syncopated treble lead over a
-                rhythmically steady bass. A ragtime composition is usually
-                composed three or four contrasting sections or strains, each one
-                being 16 or 32 measures in length.
-              </Text>
-
-              <br />
-
-              <Text>Ragtime was typically played in saloons</Text>
-            </RagtimeSection>
-            <RagtimeSection title="Who is Scott Joplin?">
-              <Text>
-                Scott Joplin was an American composer and pianist. Dubbed the
-                &quot;King of Ragtime&quot;, he composed more than 40 ragtime
-                pieces, one ragtime ballet, and two operas.
-              </Text>
-
-              <br />
-
-              <Text>
-                Scott Joplin was born around the years 1867 and 1868 in
-                Texarkana, he was the second of six children, born to Giles
-                Joplin, a former slave. Joplin was born into a musical family,
-                his father played the violin and his mother sang and played the
-                banjo. By the age of seven Joplin would start practicing on the
-                piano, and at some point he was able to receive a musical
-                education from a man called Julius Weiss
-              </Text>
-            </RagtimeSection>
-            <RagtimeSection title="What is syncopation?">
-              <picture>
-                <img src="/assets/syncopation.png" alt="" />
-              </picture>
-            </RagtimeSection>
+            <div className="flex flex-col">
+              <RagtimeSection title="Who is Scott Joplin?">
+                <Text>
+                  Scott Joplin was an American composer and pianist. Dubbed the
+                  &quot;King of Ragtime&quot;, he composed more than 40 ragtime
+                  pieces, one ragtime ballet, and two operas.
+                </Text>
+                <br />
+                <Text>
+                  Scott Joplin was born around the years 1867 and 1868 in
+                  Texarkana, he was the second of six children, born to Giles
+                  Joplin, a former slave. Joplin was born into a musical family,
+                  his father played the violin and his mother sang and played
+                  the banjo. By the age of seven Joplin would start practicing
+                  on the piano, and at some point he was able to receive a
+                  musical education from a man called Julius Weiss
+                </Text>
+              </RagtimeSection>
+              <RagtimeSection title="What is syncopation?">
+                <picture>
+                  <img src="/assets/syncopation.png" alt="" />
+                </picture>
+              </RagtimeSection>
+            </div>
           </div>
           <GoBack />
         </Animate>
