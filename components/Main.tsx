6d4af09368c4641b663865af21f6cf15b1055cda
Nicholas <contactnicholas81@gmail.com>
2024-09-20 13:45:51 +0100

    all good now

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,10 +1,15 @@
+import { CITY, COUNTRY, FIRST_NAME } from "../lib/constants";
 import { TypeAnimation } from "react-type-animation";
-import { FIRST_NAME } from "../lib/constants";
+import { FiNavigation } from "react-icons/fi";
 import { fireworks } from "../lib/fireworks";
 import { Heading } from "./Heading";
 import { Button } from "./Button";
 import { Avatar } from "./Avatar";
 import { Links } from "./Links";
+import { Tag } from "./Tag";
+
+// Next.js
+import Link from "next/link";
 
 export const Main = () => {
   const date = new Date();
@@ -25,13 +30,13 @@ export const Main = () => {
     <>
       <main className="flex md:flex-row md:flex-start flex-col-reverse items-start md:items-center w-full justify-between mb-12 mt-20">
         <div className="flex flex-col">
-          {/* <Link
-            className={`cursor-pointer p-[2px] mr-[2px] w-fit focus:ring-4 ring-[${THEME}] outline-none rounded-full duration-300`}
+          <Link
+            className={`cursor-pointer p-[2px] mr-[2px] w-fit focus:ring-4 ring-primary outline-none rounded-full duration-300`}
             href={`https://www.google.com/maps/search/${CITY}`}
             target="_blank"
           >
             <Tag
-              title={`${COUNTRY}`}
+              title={`${CITY}, ${COUNTRY}`}
               icon={
                 <FiNavigation
                   className="ml-1 hover:scale-110 duration-150"
@@ -39,20 +44,13 @@ export const Main = () => {
                 />
               }
             />
-          </Link> */}
+          </Link>
           <Heading style={{ marginBottom: "0.5rem", marginTop: "8px" }}>
             Hi, I&apos;m {FIRST_NAME}! <span className="wave">👋</span>
           </Heading>
           <div className="text-gray-700 flex font-semibold dark:text-gray-100 mb-4">
             <TypeAnimation
-              sequence={[
-                "Software Engineer",
-                2000,
-                "Entrepreneur",
-                2000,
-                "Student",
-                2000,
-              ]}
+              sequence={["Software Engineer", 2000, "Student", 2000]}
               wrapper="p"
               cursor={true}
               repeat={Infinity}
