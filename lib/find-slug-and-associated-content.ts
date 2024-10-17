3d3bae9bc32e525e3870037114984897f56926f5
Nicholas <contactnicholas81@gmail.com>
2024-10-17 15:43:39 +0100

    ignore

--- lib/find-slug-and-associated-content.ts
+++ lib/find-slug-and-associated-content.ts
@@ -1,6 +1,6 @@
 import { NextRouter } from "next/router";
 
-// NOTE: The array MUST have a 'slug' property in order for this to work.
+// The array MUST have a 'slug' property in order for this to work.
 
 export const findSlugAndAssociatedContent = <T extends { slug: string }>(
   router: NextRouter,
