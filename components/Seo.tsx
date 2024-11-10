fe782c2549f035f3b91edfded8e0b499e383dd55
Nicholas <contactnicholas81@gmail.com>
2024-11-10 12:21:07 +0000

    pog change

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -31,7 +31,7 @@ export const Seo = (props: Props) => {
       <Head>
         <title>{props.title}</title>
         <meta name="theme-color" content={THEME} />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="description" content={props.description} key="desc" />
         <meta name="twitter:title" content={props.title} />
