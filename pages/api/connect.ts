3fbababc1431fcb5e44a2b00182a9b8493ccd8c3
Nicholas <contactnicholas81@gmail.com>
2024-11-21 12:18:08 +0000

    again, fuck you brandon

--- pages/api/connect.ts
+++ pages/api/connect.ts
@@ -1,4 +1,4 @@
-// import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 // export default async function handler(
 //   req: NextApiRequest,
