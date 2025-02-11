63127968f280263b9777e7c1aa43273940671cec
Nicholas <contactnicholas81@gmail.com>
2025-02-11 15:15:19 +0000

    add og image

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -38,11 +38,16 @@ export const Seo = (props: Props) => {
         <meta name="twitter:description" content={props.description} />
         <meta property="twitter:creator" content={`@${social.x.username}`} />
         <meta property="twitter:site" content="@nick" />
+        <meta name="twitter:image" content="/og.png" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="keywords" content={seoKeywords} />
         <meta property="og:title" content={props.title} />
         <meta property="og:site_name" content={`${FULL_NAME}`} />
         <meta property="og:url" content={`https://${DOMAIN}/`} />
+        <meta property="og:image" content="/og.png" />
         <meta property="og:description" content={props.description} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="600" />
         <meta property="og:type" content="website" />
         <meta
           property="twitter:image"
