ee1f2f23cbabc3dcbd3a52505d4c1afcf455b449
Nicholas <contactnicholas81@gmail.com>
2025-10-01 17:31:44 +0100

    fix imports

--- components/dropdown-menu.tsx
+++ components/dropdown-menu.tsx
@@ -1,24 +1,16 @@
 "use client";
 
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
+import { Check, ChevronRight, Circle } from "lucide-react";
+import { cn } from "../lib/utils";
 import * as React from "react";
 
-import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
-
-import { Check, ChevronRight, Circle } from "lucide-react";
-
-import { cn } from "../lib/utils";
-
-const DropdownMenu = DropdownMenuPrimitive.Root;
-
-const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
-
-const DropdownMenuGroup = DropdownMenuPrimitive.Group;
-
-const DropdownMenuPortal = DropdownMenuPrimitive.Portal;
-
-const DropdownMenuSub = DropdownMenuPrimitive.Sub;
-
 const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
+const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
+const DropdownMenuPortal = DropdownMenuPrimitive.Portal;
+const DropdownMenuGroup = DropdownMenuPrimitive.Group;
+const DropdownMenuSub = DropdownMenuPrimitive.Sub;
+const DropdownMenu = DropdownMenuPrimitive.Root;
 
 const DropdownMenuSubTrigger = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.SubTrigger>,
