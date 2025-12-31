15b1a04c65620bb93d28687b9e197666cdf28b08
Nicholas <contactnicholas81@gmail.com>
2025-12-31 13:32:46 +0000

    use og.png image
    Co-authored-by: g6cxwm47dz-art <g6cxwm47dz@privaterelay.appleid.com>

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -49,14 +49,8 @@ export const Seo = (props: Props) => {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="600" />
         <meta property="og:type" content="website" />
-        {/* <meta
-          property="twitter:image"
-          content={`https://${DOMAIN}/branding/secondary_dark_short_sig_avatar.png`}
-        />
-        <meta
-          property="og:image"
-          content={`https://${DOMAIN}/branding/secondary_dark_short_sig_avatar.png`}
-        /> */}
+        <meta property="twitter:image" content="/og.png" />
+        <meta property="og:image" content="/og.png" />
       </Head>
       <Script async defer src="https://buttons.github.io/buttons.js" />
       {date.getMonth() + 1 >= 12 && date.getDate() >= 1 ? (
