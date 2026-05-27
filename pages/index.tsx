2aeb45e61dd5c5a2fb9fa0f0565edaa92d267dae
Nicholas <contactnicholas81@gmail.com>
2026-05-27 18:16:52 +0100

    mistake

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import { Listening } from "../components/Listening";
 import { Contact } from "../components/Contact";
 import { Animate } from "../components/Animate";
 import { License } from "../components/License";
-// import { Layout } from "../components/Layout";
+import { Layout } from "../components/Layout";
 import { Music } from "../components/Music";
 import { About } from "../components/About";
 import { Main } from "../components/Main";
