bd534b2b5062307693f40d6f82cde1779fc4be6f
Nicholas <contactnicholas81@gmail.com>
2026-07-16 01:05:54 +0100

    add avatar border effect

--- components/Main.tsx
+++ components/Main.tsx
@@ -66,7 +66,7 @@ export const Main = () => {
           ) : null}
         </div>
         <div className="flex mb-8 md:mb-0 duration-300 rounded-full">
-          <Avatar border={false} width={135} height={125} clickable={true} />
+          <Avatar border={true} width={135} height={125} clickable={true} />
         </div>
       </main>
     </>
