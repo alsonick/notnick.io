f6b2d6123be25143d14f31c8899685cb98472d9f
Nicholas <contactnicholas81@gmail.com>
2026-07-22 21:26:21 +0100

    complete

--- lib/learn/cisco-200-301/index.ts
+++ lib/learn/cisco-200-301/index.ts
@@ -15,6 +15,14 @@ export const CISCO_200_301_MODULES: Module[] = [
       {
         id: generateRandomId(),
         title: "Network Basics: What is a Network?",
+        completed: true,
+        learnt:
+          "Networks are a collection of devices that are connected together to share resources and information. They can be classified into different types based on their size, topology, and protocols used. Each device on a network has a unique identifier called an IP address, which allows it to communicate with other devices. Networks can be wired or wireless, and they can be used for various purposes such as communication, data transfer, and internet access. Devices are also assigned a MAC address, which is a unique identifier for the network interface card (NIC) of the device.",
+      },
+      {
+        id: generateRandomId(),
+        title:
+          "What is a Switch? A Router? What network is this? And what are these?",
         completed: false,
         learnt: "",
       },
